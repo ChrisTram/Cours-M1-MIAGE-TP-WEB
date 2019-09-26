1bf7c3523e8bc2f012bc2eb3ac5b139011cb5a6e
Unknown <chris.tramier97@gmail.com>
2019-09-26 15:33:33 +0200

    Added bootstrap to project and Login.vue

--- TP01Bonus/vue-firebase-chat/src/router.js
+++ TP01Bonus/vue-firebase-chat/src/router.js
@@ -15,17 +15,24 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/chat/:nickname/:roomid/:roomname',
+      name: 'Chat',
+      component: Chat
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/room/:nickname',
+      name: 'RoomList',
+      component: Room
+    },
+    {
+      path: '/add-room',
+      name: 'AddRoom',
+      component: AddRoom
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
     }
   ]
 })
