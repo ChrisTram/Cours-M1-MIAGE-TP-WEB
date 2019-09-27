b25f69f0630f6e6d896c5a19e1a8494c2c54e054
Unknown <chris.tramier97@gmail.com>
2019-09-27 19:19:30 +0200

    Firebase connection added

--- TP01Bonus/vue-firebase-chat/src/Firebase.js
+++ TP01Bonus/vue-firebase-chat/src/Firebase.js
@@ -1,11 +1,13 @@
 import * as firebase from 'firebase';
-const config = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDCS8NFKKrNOSrp9wl5fNavNWkcSKvmecM",
+  authDomain: "web-tp01-chat.firebaseapp.com",
+  databaseURL: "https://web-tp01-chat.firebaseio.com",
+  projectId: "web-tp01-chat",
+  storageBucket: "web-tp01-chat.appspot.com",
+  messagingSenderId: "737769664904",
+  appId: "1:737769664904:web:b06bcfd87dfa6757caca3d"
+};
 
-  firebase.initializeApp(config);
+  firebase.initializeApp(firebaseConfig);
   export default firebase;
